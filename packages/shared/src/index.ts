2cc1fe6c9a7afd7804abd75e4a9db7f171b38184
thissidemayur <thissidemayur@gmail.com>
2026-02-12 21:02:35 +0530

    add: use as a centeralize export hub for all export methods,type,dto,interface

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,15 @@
+export * from "./http/apiError"
+export * from "./http/apiResponse";
+export * from "./http/asyncHandler"
+
+export * from "./generated/prisma"
+
+export * from "./interface/auth.service.interface"
+export * from "./interface/member.repository.interface"
+export * from "./interface/queue.service.interface"
+export * from "./interface/session.repository.interface"
+export * from "./interface/token.service.interface"
+export * from "./interface/user.repository.interface"
+export * from "./interface/auth.controller.interface"
+
+export * from "./dtos/auth.dto"
