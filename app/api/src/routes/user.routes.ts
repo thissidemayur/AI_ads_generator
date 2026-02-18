5796ec97294c2ff1114f49b5cafbd6579504588b
thissidemayur <thissidemayur@gmail.com>
2026-02-19 04:19:45 +0530

    feat: add user routes

---
+++ app/api/src/routes/user.routes.ts
@@ -0,0 +1,51 @@
+import { Router, type RequestHandler } from "express";
+
+import { validate } from "../middlewares/validate.middleware";
+
+import { changePasswordSchema, confirmEmailChangeSchema, requestEmailChangeSchema, updateUserSchema, type IUserController } from "@project/shared";
+
+export const userRouter = (userController: IUserController, authenticate:RequestHandler) => {
+  const router = Router();
+  router.get("/me", authenticate, userController.getMe);
+  router.patch(
+    "/me",
+    authenticate,
+    validate(updateUserSchema),
+    userController.updateMe,
+  );
+
+  // Security Management
+  router.post(
+    "/change-password",
+    authenticate,
+    validate(changePasswordSchema),
+    userController.updatePassword,
+  );
+  router.post(
+    "/terminate-sessions",
+    authenticate,
+    userController.terminateSessions,
+  );
+
+  // Email Change Flow
+  router.post(
+    "/request-email-change",
+    authenticate,
+    validate(requestEmailChangeSchema),
+    userController.requestEmailChange,
+  );
+  router.post(
+    "/confirm-email-change",
+    authenticate,
+    validate(confirmEmailChangeSchema),
+    userController.emailChange,
+  );
+
+  // Account Destruction
+  router.delete("/account", authenticate, userController.deleteAccount);
+  4;
+
+  return router;
+};
+
+
