63209a7af7cc0df3b2a600620b438074d0d58722
thissidemayur <thissidemayur@gmail.com>
2026-02-19 04:14:44 +0530

    feat: Add an authorize middleware

---
+++ app/api/src/middlewares/authorize.middleware.ts
@@ -0,0 +1,22 @@
+import { ApiError } from "@project/shared";
+import { asyncHandler, type Role } from "@project/shared/server";
+import type { NextFunction, Request, Response } from "express";
+
+export const authorize = (allowedRole:Role[]) =>{
+    return asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
+        if(!req.authUser) {
+            return next(new ApiError(401,"Authentication required"))
+        }
+
+        // check user role exist
+        const hasPermission = allowedRole.includes(req.authUser.role)
+        if(!hasPermission) {
+            throw new ApiError(
+              401,
+              `Access denied. Requires one of ${allowedRole.join(", ")}`,
+            );
+        }
+
+        next()
+    })
+}
