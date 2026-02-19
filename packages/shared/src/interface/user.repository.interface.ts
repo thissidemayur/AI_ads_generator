532628932730d48121cfae10443a3c5cbcf74a6d
thissidemayur <thissidemayur@gmail.com>
2026-02-19 15:54:08 +0530

    feat(shared): implement userRepository  interface

--- packages/shared/src/interface/user.repository.interface.ts
+++ packages/shared/src/interface/user.repository.interface.ts
@@ -4,7 +4,18 @@ export interface IUserRepository {
   findByEmail(email: string): Promise<User | null>;
   findById(id: string): Promise<User | null>;
   create(data: Prisma.UserCreateInput): Promise<User>;
-  
+
   incrementVersion(userId: string): Promise<void>; // for the "Kill switch" to delete all session
   getCurrentVersion(userId: string): Promise<number>;
+
+  updatePassword(userId: string, password: string): Promise<void>;
+  createWithTx(
+    tx: Prisma.TransactionClient,
+    data: Prisma.UserCreateInput,
+  ): Promise<User>;
+
+  updateEmail(userId: string, email: string): Promise<void>;
+  delete(userId: string): Promise<void>;
+  update(userId: string, data: Partial<Prisma.UserUpdateInput>): Promise<User>;
+  deleteWithTx(tx: Prisma.TransactionClient, userId: string): Promise<void>;
 }
