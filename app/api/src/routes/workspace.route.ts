b91262232ebbda03188d7d18e717fd30761f70fa
thissidemayur <thissidemayur@gmail.com>
2026-02-19 15:21:56 +0530

    fix(api): refactors types import from @project/shared/index.ts to @project/shared/client.ts feat(api): add workspace.controller's adjacent routes

--- app/api/src/routes/workspace.route.ts
+++ app/api/src/routes/workspace.route.ts
@@ -1,21 +1,30 @@
 // apps/api/src/routes/workspace.routes.ts
 import { Router, type RequestHandler } from "express";
 import type { IWorkspaceControllerInterface } from "@project/shared";
+// import { createWorkspaceSchema, addMemberSchema } from "@project/shared/client";
+// import { validate } from "../middlewares/validate.middleware";
 
 export const workspaceRouter = (
   controller: IWorkspaceControllerInterface,
-  authenticate:RequestHandler) => {
+  authenticate: RequestHandler,
+) => {
   const router = Router();
 
-  // All workspace routes require a valid session
   router.use(authenticate);
 
   router.post("/", controller.create);
-  router.get("/my", controller.getMyWorkspace);
-  router.get("/current", controller.getWorkspaceById);
-  router.post("/members", controller.addMemberToWorkspace);
+  router.get("/my", controller.getMyWorkspace); // List of all workspaces user belongs to
+  router.get("/current", controller.getWorkspaceById); // Details of x-tenant-id
+
+  router.get("/members", controller.getMembers);
+  router.post(
+    "/members",
+    controller.addMemberToWorkspace,
+  );
+
+  router.delete("/current", controller.deleteCurrentWorkspace);
+
+  router.delete("/purge-all", controller.purgeAllWorkspaces);
 
   return router;
 };
-
-
