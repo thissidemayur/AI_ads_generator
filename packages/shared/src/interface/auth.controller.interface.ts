6f32841063afac5ce9e78690f0a1b75cf1f5f061
thissidemayur <thissidemayur@gmail.com>
2026-02-19 15:51:27 +0530

    feat(shared): implement  authController interface

--- packages/shared/src/interface/auth.controller.interface.ts
+++ packages/shared/src/interface/auth.controller.interface.ts
@@ -1,45 +1,13 @@
-import type { NextFunction, Request, Response } from "express";
+import type {  RequestHandler } from "express";
 
 export interface IAuthController {
-  register(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<Response | void>;
-
-  login(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<Response | void>;
-  verifyEmail(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<Response | void>;
-  refresh(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<Response | void>;
-  logout(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<Response | void>;
-  forgetPassword(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<Response | void>;
-  resetPassword(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<Response | void>;
-  resendVerifcation(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<Response | void>;
+  register: RequestHandler;
+  login: RequestHandler;
+  verifyEmail:RequestHandler;
+  
+  refresh: RequestHandler;
+  logout: RequestHandler;
+  forgetPassword: RequestHandler;
+  resetPassword: RequestHandler;
+  resendVerification: RequestHandler;
 }
