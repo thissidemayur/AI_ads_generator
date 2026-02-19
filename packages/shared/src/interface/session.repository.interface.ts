8e053e728891cc16eaf3097839e37c511d963da1
thissidemayur <thissidemayur@gmail.com>
2026-02-19 15:53:26 +0530

    feat(shared): implement sessionRepository interface

--- packages/shared/src/interface/session.repository.interface.ts
+++ packages/shared/src/interface/session.repository.interface.ts
@@ -1,8 +1,16 @@
+import type { Sessions } from "../database";
 
 
-export interface ISessionRepository{
-    create(data:{refreshToken:string; userId:string; expiresAt:Date}):Promise<void>
-    deleteByUserId(userId:string): Promise<void>
-    deleteByToken(hashedToken:string):Promise<void>
+export interface ISessionRepository {
+  create(data: {
+    refreshToken: string;
+    userId: string;
+    expiresAt: Date;
+  }): Promise<void>;
+  deleteByUserId(userId: string): Promise<void>;
+  deleteByToken(hashedToken: string): Promise<void>;
+  deleteAllForUser(userId: string): Promise<void>;
+  findByUserId(userId: string): Promise<Partial<Sessions>[]>;
+  deleteBySessionId(sessionId: string): Promise<void>;
 }
 
