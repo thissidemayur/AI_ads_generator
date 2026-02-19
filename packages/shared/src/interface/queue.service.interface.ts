65ad373207f1fea5a32ffe18ad8be04972f29980
thissidemayur <thissidemayur@gmail.com>
2026-02-19 15:52:57 +0530

    feat(shared): implement  Bull-mq queue  interface to send EMails jobs

--- packages/shared/src/interface/queue.service.interface.ts
+++ packages/shared/src/interface/queue.service.interface.ts
@@ -1,25 +1,21 @@
 // packages/shared/src/interfaces/queue.service.interface.ts
 
-export type EmailJobType = "VERIFY_EMAIL" | "PASSWORD_RESET" | "WELCOME";
+export type EmailJobType = "VERIFY_EMAIL" | "PASSWORD_RESET" | "WELCOME_EMAIL";
 
 export interface EmailJobDataDTO {
   to: string;
   type: EmailJobType;
+  name?: string;
   payload: {
     otp?: number;
-    name?: string;
     link?: string;
   };
 }
 
 export interface IQueueService {
-  /**
-   * Industry Standard: The producer (API) only cares about adding the job.
-   */
+
   addEmailJob(data: EmailJobDataDTO): Promise<void>;
 
-  /**
-   * Future-proofing: For AI tasks (BullMQ handles multiple queues)
-   */
+
   addAIJob?(tenantId: string, prompt: string): Promise<void>;
 }
