4cdbe9007f75257d3adaaea81d18562005e2c16f
thissidemayur <thissidemayur@gmail.com>
2026-02-19 15:52:05 +0530

    feat(shared): implement  memberRepository  interface

--- packages/shared/src/interface/member.repository.interface.ts
+++ packages/shared/src/interface/member.repository.interface.ts
@@ -9,4 +9,17 @@ export interface IMemberRepository {
   ): Promise<MemberWithTenant[]>;
   //   getRoleInTenant(userId: string): Promise<Role | null>;
   findMember(userId: string, tenantId: string): Promise<Member | null>;
+
+  create(userId: string, tenantId: string, role: Role): Promise<Member>;
+
+  createWithTransaction(
+    tx: Prisma.TransactionClient,
+    userId: string,
+    tenantId: string,
+    role: Role,
+  ): Promise<Member>;
+
+  findByUser(userId: string): Promise<MemberWithTenant[]>;
+  findByWorkspace(tenantId: string): Promise<Member[]>;
+  countByWorkspace(tenantId: string): Promise<number>;
 }
