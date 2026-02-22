5c80512ed10b04780801b0912fe58bbb3c8e5e2c
thissidemayur <thissidemayur@gmail.com>
2026-02-22 09:03:57 +0530

    feat(web): add setCurrentWorksapce method  in worskace-zustand store

--- app/web/src/store/workspaceStore.ts
+++ app/web/src/store/workspaceStore.ts
@@ -3,42 +3,49 @@ import { Tenant } from "@project/shared/server";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-
 interface IWorkspaceStore {
-    currentWorkspace: Tenant | null
-    userRole: UserRole | null
-    workspaces: Tenant[]
-    isLoading:boolean
+  currentWorkspace: Tenant | null;
+  userRole: UserRole | null;
+  workspaces: Tenant[];
+  isLoading: boolean;
 
-    // actions
-    setWorkspaces: (workspaces:Tenant[])=>void
-    setActiveWorkspace:(workspace:Tenant, role:UserRole) => void
-    clearWorkspace: ()=>void
+  setWorkspaces: (workspaces: Tenant[]) => void;
+  setCurrentWorkspace: (workspace: Tenant) => void; //add new method
+  setActiveWorkspace: (workspace: Tenant, role: UserRole) => void;
+  clearWorkspace: () => void;
 }
 
+export const useWorkspaceStore = create<IWorkspaceStore>()(
+  persist(
+    (set) => ({
+      currentWorkspace: null,
+      userRole: null,
+      workspaces: [],
+      isLoading: false,
 
-export const useWorkspaceStore = create < IWorkspaceStore>()(
-    persist(
-        (set)=>({
-            currentWorkspace:null,
-            userRole:null,
-            workspaces:[],
-            isLoading:false,
+      setWorkspaces: (workspaces) => set({ workspaces }),
 
-            setWorkspaces:(workspaces) => set({workspaces}),
-            setActiveWorkspace:(workspace,role)=>set({
-                currentWorkspace:workspace,
-                userRole:role
-            }),
+      setCurrentWorkspace: (workspace) =>
+        set({
+          currentWorkspace: workspace,
+        }),
 
-            clearWorkspace:()=>set({
-                workspaces:[],
-                currentWorkspace:null,
-                userRole:null
-            }),
-        }),{
-            name:'workspace-storage',
-            storage:createJSONStorage(()=>localStorage),
-        }
-    )
-)
+      setActiveWorkspace: (workspace, role) =>
+        set({
+          currentWorkspace: workspace,
+          userRole: role,
+        }),
+
+      clearWorkspace: () =>
+        set({
+          workspaces: [],
+          currentWorkspace: null,
+          userRole: null,
+        }),
+    }),
+    {
+      name: "workspace-storage",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
