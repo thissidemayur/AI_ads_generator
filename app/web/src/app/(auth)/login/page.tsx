0bde930b89288a0062492b52f1a95bd8aec96c2b
thissidemayur <thissidemayur@gmail.com>
2026-02-23 11:38:40 +0530

    feat(web): add logic to show  session_expired reson during redirects

--- app/web/src/app/(auth)/login/page.tsx
+++ app/web/src/app/(auth)/login/page.tsx
@@ -1,9 +1,16 @@
 import { LoginForm } from "@/features/auth/compononts/loginForm";
 
-export default function Page () {
-    return (
-        <>
-            <LoginForm />
-        </>
-    )
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const params = await searchParams;
+  const errorType = params.error as string | undefined;
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <LoginForm errorMessage={errorType} />
+    </div>
+  );
 }
