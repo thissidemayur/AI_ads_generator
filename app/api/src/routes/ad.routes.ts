3ea0bf423030186f24028548f05208044a477e6a
thissidemayur <thissidemayur@gmail.com>
2026-02-26 07:20:30 +0530

    feat(api): implement ads routes

--- app/api/src/routes/ad.routes.ts
+++ app/api/src/routes/ad.routes.ts
@@ -1,25 +1,45 @@
-// import {  forgetPasswordSchema, loginSchema, registerSchema, resendVerificationSchema, resetPasswordSchema, verifyEmailSchema, type IAuthController } from "@project/shared";
-// import { Router } from "express";
-// import { validate } from "../middlewares/validate.middleware";
-
-import type { IAuthController, ITokenServcice } from "@project/shared";
+import type {
+  IAdController,
+  ITokenServcice,
+} from "@project/shared";
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import type Redis from "ioredis";
 import { authorize } from "../middlewares/authorize.middleware";
 import { Role } from "@project/shared/server";
 
+export const adsRouter = (
+  controller: IAdController,
+  toknService: ITokenServcice,
+  redis: Redis,
+) => {
+  const router = Router();
+  const auth = authMiddleware(toknService, redis);
+  router.get("/", auth, controller.handleGetHistory);
 
-export const adsRouter = (controller: IAuthController,toknService:ITokenServcice,redis:Redis) =>{
-    const router = Router()
+  router.post(
+    "/generate",
+    auth,
+    authorize([Role.ADMIN, Role.EDITOR, Role.OWNER]),
+    controller.handleStartGeneration,
+  );
 
-    router.get("/",authMiddleware(toknService,redis))
+  router.get("/status/:adId", controller.handleStreamStatus);
 
-    router.post("/",authMiddleware(toknService,redis), authorize([Role.ADMIN,Role.EDITOR,Role.OWNER]))
+  router.post(
+    "/:adId/retry",
+    auth,
+    authorize([Role.ADMIN, Role.EDITOR, Role.OWNER]),
+    controller.handleRetry,
+  );
+  router.get("/:adId", auth, controller.handleGetDetails);
+  router.get("/status/:adId");
+  router.delete(
+    "/:adId",
+    auth,
+    authorize([Role.ADMIN, Role.OWNER]),
+    controller.handleDeleteAd,
+  );
 
-    router.delete(
-      "/",
-      authMiddleware(toknService, redis),
-      authorize([Role.ADMIN, Role.OWNER]),
-    );
-}
+  return router;
+};
