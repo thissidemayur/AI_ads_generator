af7261e3bbc8528fa84c67761514d29c63bfbeef
thissidemayur <thissidemayur@gmail.com>
2026-02-26 07:26:22 +0530

    feat(shared): add trnasacition parameter while creating workspace

--- packages/shared/src/interface/workspace.service.interface.ts
+++ packages/shared/src/interface/workspace.service.interface.ts
@@ -5,7 +5,11 @@ export interface IWorkspaceService {
   getUserWorkspaces(userId: string): Promise<Member[]>;
   getAllMembers(tenantId: string): Promise<Member[]>;
   addMember(tenantId: string, userId: string, role: Role): Promise<Member>;
-  createWorkspace(userId: string, name: string): Promise<Tenant>;
+  createWorkspace(
+    userId: string,
+    name: string,
+    tx: Prisma.TransactionClient,
+  ): Promise<Tenant>;
   deleteWorkspace(tenantId: string, userId: string): Promise<void>;
   deleteAllOwnedWorkspaces(userId: string): Promise<void>;
   updateWorkspaceName(
