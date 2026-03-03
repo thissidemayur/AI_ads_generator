edfb9f16e65c50b568eb19c94b919fe86223960e
thissidemayur <thissidemayur@gmail.com>
2026-03-03 11:09:26 +0530

    feat(shared): add ai service

--- packages/shared/src/interface/ai.interface.ts
+++ packages/shared/src/interface/ai.interface.ts
@@ -1,7 +1,15 @@
 import type { AdType } from "../database"
 
 export interface IAIService {
-  refinePrompt(userInput: string, aspectRatio: string): Promise<string>;
+  refinePrompt(
+    userInput: string,
+    aspectRatio: string,
+    format: AdType,
+  ): Promise<string>;
 
-  generateAd(refinePrompt: string, type: AdType): Promise<Buffer>;
+  generateAd(
+    refinePrompt: string,
+    type: AdType,
+    aspectRatio: string,
+  ): Promise<Buffer>;
 }
