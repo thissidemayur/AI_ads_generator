c15a52f598422ccdf61d069bebdb096a86ea09bd
thissidemayur <thissidemayur@gmail.com>
2026-03-03 10:20:00 +0530

    refactor(api): refactor code and add a catch block to find out malformed jwt(like  refreshToken not present)

--- app/api/src/middlewares/auth.middleware.ts
+++ app/api/src/middlewares/auth.middleware.ts
@@ -3,36 +3,46 @@ import { asyncHandler } from "@project/shared/server";
 import type { NextFunction, Request, Response } from "express";
 import type Redis from "ioredis";
 
-export const authMiddleware = (tokenService: ITokenServcice,redis:Redis) =>{
-    return asyncHandler(async(req:Request,res:Response,next:NextFunction) =>{
+export const authMiddleware = (tokenService: ITokenServcice, redis: Redis) => {
+  return asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
       const authHeader = req.headers.authorization;
       if (!authHeader?.startsWith("Bearer ")) {
         throw new ApiError(401, "Authentication required");
       }
 
-      const tenantId = req.headers["x-tenant-id"] as string
-      if(!tenantId) throw new ApiError(400,"Workspace context (x-tenant-id) missing")
+      const tenantId = req.headers["x-tenant-id"] as string;
+      if (!tenantId)
+        throw new ApiError(400, "Workspace context (x-tenant-id) missing");
 
       const token = authHeader.split(" ")[1] as string;
-      
-      const decoded = tokenService.verifyAccessToken(token);
 
-      // 2. Check "Blacklist" or "Version" in Redis
-    const cachedVersion = await redis.get(`user:version:${decoded.userId}`);
-
-    if (cachedVersion && parseInt(cachedVersion) > decoded.version) {
-      throw new ApiError(
-        401,
-        "Session expired due to security changes. Please login again.",
+      let decoded;
+      try {
+        decoded = tokenService.verifyAccessToken(token);
+      } catch (error: any) {
+        // Catch "jwt malformed", "jwt expired", etc.
+        console.error("[JWT_VERIFY_ERROR]: ", error.message);
+        throw new ApiError(401, "Invalid or expired session token");
+      }
+      // check "Blacklist" or "Version" in Redis
+      const cachedVersion = await redis.get(
+        `auth:user-version:${decoded.userId}`,
       );
-    }
-
-      req.authUser = {
-        userId:decoded.userId,
-        tenantId:decoded.tenantId,
-        role:decoded.role 
+      if (cachedVersion && parseInt(cachedVersion) > decoded.version) {
+        throw new ApiError(
+          401,
+          "Session expired due to security changes. Please login again.",
+        );
       }
 
-      next()
-    })
-}
+      req.authUser = {
+        userId: decoded.userId,
+        tenantId: decoded.tenantId,
+        role: decoded.role,
+      };
+
+      next();
+    },
+  );
+};
