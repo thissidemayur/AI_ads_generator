363aa04203774ed56ec5438fa472b27d93b41c3d
thissidemayur <thissidemayur@gmail.com>
2026-03-03 10:34:52 +0530

    feat(api): add ad's service, controller and router in container.ts to free from dependecy Injection

--- app/api/src/container.ts
+++ app/api/src/container.ts
@@ -13,6 +13,11 @@ import { UserService } from "./services/user.service";
 import { UserController } from "./controllers/user.controller";
 import { WorkspaceController } from "./controllers/workspace.controller";
 import { authMiddleware } from "./middlewares/auth.middleware";
+import { PrismaAdRepository } from "./repositories/prisma/ad.repository";
+import { AdService } from "./services/ads.service";
+import { AdController } from "./controllers/ad.controller";
+import { SSEManager } from "./services/sse.manager";
+import { RedisNotifierService } from "./services/redisNotifier.service";
 
 /**
  * Manual Composition Root
@@ -20,21 +25,24 @@ import { authMiddleware } from "./middlewares/auth.middleware";
 export const createContainer = (db: PrismaClient, redis: Redis) => {
   // initalise low level service
   const tokenService = new TokenService();
-  const queueServie = new QueueService();
-const authenticate = authMiddleware(tokenService,redis)
+  const queueService = new QueueService();
+  const authenticate = authMiddleware(tokenService, redis);
+  const sseManager = new SSEManager();
+  const notifier = new RedisNotifierService(redis);
+
   // initalize repositories
   const userRepo = new PrismaUserRepository(db);
   const memberRepo = new PrismaMemberRepository(db);
   const sessionRepo = new PrismaSessionRepository(db);
   const workspaceRepo = new PrismaWorkspaceRepository(db);
+  const adRepo = new PrismaAdRepository(db);
 
   const workspaceService = new WorkspaceService(db, workspaceRepo, memberRepo);
-
   // Initalise the service
   const authService = new AuthService(
     db,
     userRepo,
-    queueServie,
+    queueService,
     sessionRepo,
     redis,
     tokenService,
@@ -42,29 +50,29 @@ const authenticate = authMiddleware(tokenService,redis)
     workspaceService,
   );
 
+  const adService = new AdService(adRepo, queueService, notifier);
 
   const userService = new UserService(
     db, // PrismaClient
     userRepo, // IUserRepository
     workspaceRepo, // IWorkspaceRepository
     redis, // Redis
-    queueServie, // QueueService
+    queueService, // QueueService
     memberRepo, // IMemberRepository
     sessionRepo, // ISessionRepository
   );
 
-  
   // initalize the controlleer
-  
+
   const authController = new AuthController(authService);
   const userController = new UserController(userService);
-  const workspaceController = new WorkspaceController(workspaceService)
-
-
+  const workspaceController = new WorkspaceController(workspaceService);
+  const adController = new AdController(adService, sseManager);
   return {
     authController,
     userController,
     workspaceController,
+    adController,
     // service
     authService,
     workspaceService,
