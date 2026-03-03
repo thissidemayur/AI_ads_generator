4d800658983b315f3642df8beaa05ee3d39449ad
thissidemayur <thissidemayur@gmail.com>
2026-03-03 10:52:59 +0530

    feat(worker): add container to maintain depedency circular inejctin problem

--- app/worker/src/container.ts
+++ app/worker/src/container.ts
@@ -3,13 +3,14 @@ import { AdRepository } from "./repository/ads.repository";
 import { CloudinaryStorageService } from "./service/cloudinary.storage.service";
 import { redis } from "./config/redis";
 import { prismaConnection } from "@project/shared/server";
-import { AiService } from "./service/ai.service";
+import { AiService } from "@project/shared";
 
 const notifier = new RedisNotifierService(redis)
 const adRepo = new AdRepository(prismaConnection)
 const cloudinaryStorage = new CloudinaryStorageService()
 
-const aiService = new AiService()
+let apiKey = process.env.GEMINI_API_KEY;
+const aiService = new AiService(apiKey = apiKey!)
 
 export const dependencies = {
     notifier,
