e7547412f6599c1f48cf591b38eb61d25c4db6f3
thissidemayur <thissidemayur@gmail.com>
2026-03-03 10:33:47 +0530

    feat(api): add ads router in app.ts also refactor the file using pretiier

--- app/api/src/app.ts
+++ app/api/src/app.ts
@@ -1,28 +1,40 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import { authRouter } from "./routes/auth.routes"
-import cors from "cors"
-import { env } from "./config/env"
-import { errorMiddleware } from "./middlewares/ErrorHandler.middleware"
-import { userRouter } from "./routes/user.routes"
-import type { createContainer } from "./container"
-import { workspaceRouter } from "./routes/workspace.route"
+import express from "express";
+import cookieParser from "cookie-parser";
+import { authRouter } from "./routes/auth.routes";
+import cors from "cors";
+import { env } from "./config/env";
+import { errorMiddleware } from "./middlewares/ErrorHandler.middleware";
+import { userRouter } from "./routes/user.routes";
+import type { createContainer } from "./container";
+import { workspaceRouter } from "./routes/workspace.route";
+import { adsRouter } from "./routes/ad.routes";
 
-export const createApp = (container:ReturnType<typeof createContainer>) =>{
-    const app = express()
+export const createApp = (container: ReturnType<typeof createContainer>) => {
+  const app = express();
 
-    // use middleware
-    app.use(cors({
-        origin:env.FRONTEND_URL,
-        credentials:true // allow cookies to be sent/recieved
-    }))
-    app.use(express.json())
-    app.use(cookieParser())
+  app.use(
+    cors({
+      origin: env.FRONTEND_URL,
+      credentials: true,
+    }),
+  );
+  app.use(express.json());
+  app.use(cookieParser());
 
-    app.use("/api/auth", authRouter(container.authController));
-    app.use("/api/user",userRouter(container.userController,container.authenticate))
-    app.use("/api/workspaces",workspaceRouter(container.workspaceController,container.authenticate));
-    // global Error handler
-    app.use(errorMiddleware)
-    return app
-}
+  app.use("/api/auth", authRouter(container.authController));
+  app.use(
+    "/api/user",
+    userRouter(container.userController, container.authenticate),
+  );
+  app.use(
+    "/api/workspaces",
+    workspaceRouter(container.workspaceController, container.authenticate),
+  );
+  app.use(
+    "/api/ads",
+    adsRouter(container.adController, container.authenticate),
+  );
+  // global Error handler
+  app.use(errorMiddleware);
+  return app;
+};
